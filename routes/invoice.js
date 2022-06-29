416960e7154c4e445e860d0f7f76cdd6282ec533
peggylicious <p.asq@live.com>
2022-06-29 14:54:38 +0100

    Add router to get all invoices

--- routes/invoice.js
+++ routes/invoice.js
@@ -41,4 +41,15 @@ router.post("/create", (req, res, next) => {
     });
 });
 
+router.get("/all", (req, res, next) => {
+  invoice
+    .find()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
