4dfd4cb37288050ee20538d21f0bd6cb116a6d5d
peggylicious <p.asq@live.com>
2022-07-04 14:32:41 +0100

    Update user model

--- models/user.js
+++ models/user.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const userSchema = mongoose.Schema({
   user_name: String,
+  email: String,
+  password: String,
   deleted_invoice_id: Array,
 //   items: [],
   last_invoice_digit: Number,
-  company_name: String
+  company_name: String,
 });
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("User", userSchema);
